a584c2a937d16da2e0a5e212447b23964c974cbb
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-02-20 16:17:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = os.environ.get("API_ID", "9254436")
 
 API_HASH = os.environ.get("API_HASH", "42665ffe4407fbc3f59c412caa9d84d3")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5999105365:AAGHarGuzulh9f9HxX23PM4eTCgZIZAcwCQ") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5999105365:AAHdxTZa0_-Ziq-wcEJyTGNZcXPFWoZe1ZI") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "MLZ_BOTZ") 
 
