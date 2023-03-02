6427da16694861c43e8e57f5c9421d3b42a15089
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-02 15:04:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://sui:sui@cluster0.fmobrpu.mongodb
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/62472f793d410e849b40b.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1833209093 5845960615 5596825598').split()]
 
