4316f37e2a688481af7cc96d75ce25c0d85c9bb3
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-23 15:37:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,4 +27,8 @@ PORT = os.environ.get('PORT', '8080')
 
 LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001898159090'))
 
-COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+BANNED_USERS = filters.user()
+
+BOT_NAME = getenv("MUSIC_BOT_NAME", "MLZ Rᴇɴᴀᴍᴇ ʙᴏᴛ")
+
+PING_URL = getenv("PING_URL", "https://telegra.ph/file/56d1760224589ee370186.jpg")
